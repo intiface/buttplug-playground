05ce5217971889753d3a93ebb880b23738513a82
Kyle Machulis <kyle@nonpolynomial.com>
2018-04-26 20:39:52 -0700

    feat: Try adding matomo tracking to app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,22 @@ const Icon = require("vue-awesome/components/Icon");
 const VueMaterial = require("vue-material");
 const VueTouch = require("vue-touch");
 import * as ButtplugPanel from "vue-buttplug-material-component";
+const MatomoTracker = require('matomo-tracker');
+
+// Initialize with your site ID and Matomo URL
+const matomo = new MatomoTracker(12, "https://apps.nonpolynomial.com/p/js/", true);
+
+// Optional: Respond to tracking errors
+matomo.on("error", function(err: string) {
+  console.log("error tracking request: ", err);
+});
+
+// Track a request URL:
+// Either as a simple string …
+matomo.track({
+  url: "https://buttplug.world/playground",
+  action_name: "Buttplug Playground",
+});
 
 // Fix viewport scaling on iOS
 require("viewport-units-buggyfill").init();
