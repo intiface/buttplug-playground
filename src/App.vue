fbfa876d57754a409b19b2a27651b8c48867be12
Kyle Machulis <kyle@nonpolynomial.com>
2018-06-01 17:22:37 -0700

    chore: Extract patreon button CSS into its own file

--- src/App.vue
+++ src/App.vue
@@ -85,9 +85,11 @@
 <script lang="ts" src="./App.ts">
 </script>
 
-<style src="vuetify/dist/vuetify.min.css"></style>
-
 <style lang="css">
+ /********************************/
+ /* Fonts */
+ /********************************/
+
  @font-face {
    font-family: 'Material Icons';
    font-style: normal;
@@ -122,32 +124,15 @@
    font-feature-settings: 'liga';
  }
 
+ /********************************/
+ /* Basic HTML styles */
+ /********************************/
+ 
  html, body {
    margin: 0;
    padding: 0;
    height: 100vh;
    width: 100vw;
- }
-
- #app {
-   height: 100%;
-   width: 100%;
-   font-size: 16px;
-   font-weight: 400;
-   text-align: left;
-   text-transform: none;
-   font-family: Roboto,Noto Sans,Noto,sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   color: #2c3e50;
- }
-
- /* Make our touch wrapper div take up the whole screen, but also make it
-    fixed so that we don't have problems with readjustment snapping */
- #gesture-wrapper {
-   position: fixed;
-   height: 100%;
-   width: 100%;
  }
 
  h1, h2 {
@@ -168,32 +153,29 @@
    color: #42b983;
  }
 
- .md-input-container.md-has-value input {
-   font-size:10pt;
- }
+ /********************************/
+ /* App container styles */
+ /********************************/
 
- md-sidenav,
- md-sidenav.md-locked-open,
- md-sidenav.md-closed.md-locked-open-add-active {
-   min-width: 200px !important;
-   width: 85vw !important;
-   max-width: 400px !important;
- }
-
- .md-tabs .md-tab  {
-   padding: 0;
- }
-
- .haptics-info {
-   font-size: 14px;
- }
-
- #hamburger-start-text {
+ /* Make our touch wrapper div take up the whole screen, but also make it
+    fixed so that we don't have problems with readjustment snapping */
+ #gesture-wrapper {
    position: fixed;
-   font-size:25px;
-   z-index:50;
-   top: 33px;
-   left: 85px;
+   height: 100%;
+   width: 100%;
+ }
+
+ #app {
+   height: 100%;
+   width: 100%;
+   font-size: 16px;
+   font-weight: 400;
+   text-align: left;
+   text-transform: none;
+   font-family: Roboto,Noto Sans,Noto,sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   color: #2c3e50;
  }
 
  #swipe-start-text {
@@ -202,6 +184,10 @@
    left: 40px;
    position: fixed;
  }
+
+ /********************************/
+ /* Nav Drawer opener styles */
+ /********************************/
 
  #sidetab-aligner {
    height: 100vh;
@@ -233,53 +219,14 @@
    cursor: pointer;
  }
 
+ /********************************/
+ /* Misc application styles */
+ /********************************/
+
  #playground-container {
    width: 80%;
    margin: auto;
  }
-
- .syncydink-nav-file-input {
-   max-width: 95%;
-   margin: auto;
- }
-
- ._2KV-widgets-shared--patreonWidgetWrapper {
-   color: #052D49;
-   font-family: 'America', 'GT America', 'Lato', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
-   font-size: 16px;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   text-rendering: optimizeLegibility;
- }
-
- /* Patreon button rendering CSS */
-
- #patreon-button {
-   position: absolute;
-   bottom: 0;
-   right: 0;
- }
-
- /* sc-component-id: sc-keyframes-iECmZH */
- @-webkit-keyframes iECmZH{0%{-webkit-transform:rotate(0deg);-ms-transform:rotate(0deg);transform:rotate(0deg);}100%{-webkit-transform:rotate(360deg);-ms-transform:rotate(360deg);transform:rotate(360deg);}}@keyframes iECmZH{0%{-webkit-transform:rotate(0deg);-ms-transform:rotate(0deg);transform:rotate(0deg);}100%{-webkit-transform:rotate(360deg);-ms-transform:rotate(360deg);transform:rotate(360deg);}}
- /* sc-component-id: sc-htpNat */
- .sc-htpNat {}
- .gdWQYu{visibility:visible;}
- /* sc-component-id: sc-bxivhb */
- .sc-bxivhb {}
- .ffInCX{-webkit-backface-visibility:hidden;backface-visibility:hidden;background-color:#F96854;border:2px solid #F96854;border-radius:0;box-sizing:border-box;color:#FFFFFF !important;display:inline-block;font-size:0.8090234857849197rem !important;font-weight:700;padding:0.5rem 0.75rem;position:relative;text-align:center;text-decoration:none;text-transform:uppercase;-webkit-transition:all 300ms cubic-bezier(0.19,1,0.22,1);transition:all 300ms cubic-bezier(0.19,1,0.22,1);-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;white-space:nowrap;cursor:pointer;}.ffInCX:focus{box-shadow:0 0 8px 0 #358EFF;outline:none;}.ffInCX:hover{background-color:#FA7664;border-color:#FA7664;box-shadow:0 0.25rem 0.75rem rgba(5,45,73,0.09999999999999998);}.ffInCX:active{box-shadow:none;-webkit-transform:translateY(0);-ms-transform:translateY(0);transform:translateY(0);}
- /* sc-component-id: sc-gzVnrw */
- .sc-gzVnrw {}
- .dJCpyC{-webkit-align-content:flex-start;-ms-flex-line-pack:flex-start;align-content:flex-start;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-grow:initial;-ms-flex-grow:initial;flex-grow:initial;-webkit-flex-wrap:nowrap;-ms-flex-wrap:nowrap;flex-wrap:nowrap;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;styled-components:bug-fix;}@media (min-width:1rem){.dJCpyC{-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;}}
- /* sc-component-id: sc-htoDjs */
- .sc-htoDjs {}
- .fqfmvk{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;}.fqfmvk svg{-webkit-align-self:center;-ms-flex-item-align:center;align-self:center;height:0.75rem;width:0.75rem;}.fqfmvk svg *[data-color='1']{fill:#FFFFFF;-webkit-transition:all 300ms cubic-bezier(0.19,1,0.22,1);transition:all 300ms cubic-bezier(0.19,1,0.22,1);}.fqfmvk svg *[data-color='2']{fill:#052D49;-webkit-transition:all 300ms cubic-bezier(0.19,1,0.22,1);transition:all 300ms cubic-bezier(0.19,1,0.22,1);}
- /* sc-component-id: sc-dnqmqq */
- .sc-dnqmqq {}
- .llsQFn{-webkit-align-self:center;-ms-flex-item-align:center;align-self:center;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;display:-webkit-inline-box !important;display:-webkit-inline-flex !important;display:-ms-inline-flexbox !important;display:inline-flex !important;padding:NaNrem;}
- /* sc-component-id: sc-gqjmRU */
- .sc-gqjmRU {}
- .fFOxVX{width:0.75rem;height:1px;}
 
  .vue-slider {
    margin-top: 20px;
@@ -301,3 +248,6 @@
    text-align: center;
  }
 </style>
+
+<style src="vuetify/dist/vuetify.min.css"></style>
+<style src="./PatreonButton.css"></style>
